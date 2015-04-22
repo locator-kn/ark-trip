e615223f04cdf0fd80fd1623dff3b500958c449d
T1m1 <Ruprecht.t@gmx.de>
2015-04-22 10:21:53 +0200

    backend-trip-3 #3 implement search route

--- src/plugin.ts
+++ src/plugin.ts
@@ -84,6 +84,19 @@ class Trip {
     };
 
     private _register(server, options) {
+        // search for trips
+        server.route({
+            method: 'GET',
+            path: '/trips/search',
+            config: {
+                handler: (request, reply) => {
+                    // implement search function
+                },
+                description: 'Search for trips',
+                tags: ['api', 'trip']
+            }
+        });
+
         // get all trips
         server.route({
             method: 'GET',
