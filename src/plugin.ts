515459d5b411f53af6c0466a149f0362ab678af5
T1m1 <Ruprecht.t@gmx.de>
2015-05-08 10:16:06 +0200

    move seearch function to new class and use new object to handle search

--- src/plugin.ts
+++ src/plugin.ts
@@ -1,3 +1,5 @@
+import Search from './util/search';
+
 export interface IRegister {
     (server:any, options:any, next:any): void;
     attributes?: any;
@@ -10,132 +12,8 @@ class Trip {
     joi:any;
     tripSchemaPost:any;
     tripSchemaPUT:any;
+    search:any;
 
-    viewName_Search = '_design/search';
-
-    // TODO: fix compiler errors for 'emit', 'getRow' and 'send'
-    // TODO: performance measurement for relevance check
-    // list for couchdb the search a trip
-    searchList = {
-        views: {
-            city: {
-                "map": function (doc) {
-                    if (doc.type == 'trip') {
-                        emit(doc.city, doc);
-                    }
-                }
-            }
-        },
-        lists: {
-            searchlist: function (head, req) {
-                var RELEVANCE_CONFIG = {
-                    RELEVANCE_SUM: 1,
-                    RELEVANCE_MOODS: 0.4,
-                    RELEVANCE_DAYS: 0.2,
-                    RELEVANCE_PERSONS: 0.2,
-                    RELEVANCE_BUDGET: 0.1,
-                    RELEVANCE_ACCOMMODATIONS: 0.1
-                };
-                var row;
-                var result = [];
-                var queryParams = JSON.stringify(req.query);
-                while (row = getRow()) {
-                    // city param is required
-                    if (queryParams != '{}' && (row.key == req.query.city)) {
-                        var moods_relevance;
-                        var possibleRelevance;
-                        // status object for relevance check
-                        var relevance = {
-                            moods: 0,
-                            budget: 0,
-                            persons: 0,
-                            days: 0,
-                            accommodations: 0
-                        };
-                        // set 'toPush' variable true and set it only false, if required param like mood don't hit
-                        var toPush = true;
-                        if (req.query.moods) {
-                            // set 'toPush' false -> don't add row to result list
-                            toPush = false;
-                            // object for relevance calculation of moods
-                            moods_relevance = {
-                                moods_sum: 0,
-                                moods_hit: 0
-                            };
-                            // split params to get all moods
-                            var moods = req.query.moods.split('_');
-                            // check if trip contains requried mood
-                            moods.forEach(function (mood) {
-                                moods_relevance.moods_sum++;
-                                if (row.value.category.indexOf(mood) > -1) {
-                                    toPush = true;
-                                    moods_relevance.moods_hit++;
-                                }
-                            });
-                        }
-                        // do not check date, if mood check don't successful..
-                        if (toPush) {
-                            if (req.query.start_date && req.query.end_date) {
-                                // check date range of trip
-                                if (req.query.start_date > row.value.start_date || req.query.end_date < row.value.end_date) {
-                                    // don't add trip if is out of range
-                                    toPush = false;
-                                }
-                            }
-                        }
-                        // if date and moods okay, check further params for relevance calculation
-                        if (toPush) {
-                            if (req.query.moods) {
-                                possibleRelevance = RELEVANCE_CONFIG.RELEVANCE_MOODS;
-                                relevance.moods = RELEVANCE_CONFIG.RELEVANCE_MOODS / moods_relevance.moods_sum * moods_relevance.moods_hit;
-                            }
-                            if (req.query.budget) {
-                                possibleRelevance += RELEVANCE_CONFIG.RELEVANCE_BUDGET;
-                                if (req.query.budget <= row.value.budget) {
-                                    relevance.budget = RELEVANCE_CONFIG.RELEVANCE_BUDGET;
-                                }
-                            }
-                            if (req.query.persons) {
-                                possibleRelevance += RELEVANCE_CONFIG.RELEVANCE_PERSONS;
-                                if (req.query.persons <= row.value.budget) {
-                                    relevance.persons = RELEVANCE_CONFIG.RELEVANCE_BUDGET;
-                                }
-                            }
-                            if (req.query.days) {
-                                possibleRelevance += RELEVANCE_CONFIG.RELEVANCE_DAYS;
-                                if (req.query.days <= row.value.days) {
-                                    relevance.days = RELEVANCE_CONFIG.RELEVANCE_DAYS;
-                                }
-                            }
-                            if (req.query.accommodations) {
-                                possibleRelevance += RELEVANCE_CONFIG.RELEVANCE_ACCOMMODATIONS;
-                                if (req.query.accommodations <= row.value.accommodations) {
-                                    relevance.accommodations = RELEVANCE_CONFIG.RELEVANCE_ACCOMMODATIONS;
-                                }
-                            }
-
-                            // relevance calculation
-                            var total = 0;
-                            for (var property in relevance) {
-                                total += relevance[property];
-                            }
-                            row.value.relevance = (total * 100 / possibleRelevance);
-
-                            // push relevant trip to result array
-                            result.push(row.value);
-                        }
-                    }
-                }
-
-                if(req.query.limit) {
-                    // TODO: sort array by relevance
-                    // TODO: send only 'limit' trips
-                }
-
-                send(JSON.stringify(result))
-            }
-        }
-    };
 
     constructor() {
         this.register.attributes = {
@@ -146,6 +24,7 @@ class Trip {
         this.boom = require('boom');
         this.joi = require('joi');
         this.initSchemas();
+        this.search = new Search();
     }
 
 
@@ -312,7 +191,7 @@ class Trip {
             path: '/trips/setup',
             config: {
                 handler: (request, reply) => {
-                    this.db.createView(this.viewName_Search, this.searchList, (err, msg)=> {
+                    this.db.createView(this.search.viewName_Search, this.search.searchList, (err, msg)=> {
                         if (err) {
                             return reply(this.boom.wrap(err, 400));
                         } else {
