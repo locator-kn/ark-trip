7a96949a97c4abc65eaa89f20eca7727008eaca6
T1m1 <Ruprecht.t@gmx.de>
2015-05-10 01:32:09 +0200

    bugfix - init variable with 0

--- src/util/search.ts
+++ src/util/search.ts
@@ -40,7 +40,7 @@ class Search {
                     // city param is required
                     if (queryParams != '{}' && (row.key == req.query.city)) {
                         var moods_relevance;
-                        var possibleRelevance;
+                        var possibleRelevance = 0;
                         // status object for relevance check
                         var relevance = {
                             moods: 0,
