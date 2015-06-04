b85ede57781582cc8eb40f0c281eb9157184eee1
SteffenGorenflo <SteffenGorenflo@gmail.com>
2015-06-04 22:47:28 +0200

    refactored schema of trip and added days

--- src/util/schema.ts
+++ src/util/schema.ts
@@ -22,41 +22,50 @@ class Schema {
      *    We need two schemas because PUT required '_id' and '_rev'.
      */
     private initSchemas():void {
-        // basic trip schema
-        var trip = this.joi.object().keys({
+
+        // required trip schema document
+        var tripSchema = this.joi.object().keys({
             title: this.joi.string().required(),
-            description: this.joi.string().required(),
-            description_money: this.joi.string(),
+
             city: this.joi.object().keys({
                 title: this.joi.string().required(),
                 place_id: this.joi.string().required(),
                 id: this.joi.string().required()
+            }).required(),
+
+            description: this.joi.string().required(),
+
+            description_money: this.joi.string(),
+
+            // if equipment is provided, this key must be true
+            accommodation: this.joi.when('accommodation_equipment', {
+                is: this.joi.array().required(),
+                then: this.joi.valid(true).required(),
+                otherwise: this.joi.boolean().required()
             }),
-            start_date: this.joi.date(),
-            end_date: this.joi.date(),
-            accommodation: this.joi.boolean().required(),
             accommodation_equipment: this.joi.array(),
+
+            start_date: this.joi.date().min('now').required(),
+            end_date: this.joi.date().min(this.joi.ref('start_date')).required(),
+
+            persons: this.joi.number().integer().min(1).required(),
+
+            // TODO: validate max days : https://github.com/hapijs/joi/issues/667
+            days: this.joi.number().integer().min(1),
+
+            // optional keys upon creation
             moods: this.joi.array(),
-
-
             locations: this.joi.array(),
-
-            // TODO: required?
-            persons: this.joi.number().integer(),
 
             // TODO: Frontend?
             active: this.joi.boolean().default(true),
             delete: this.joi.boolean().default(false),
         });
 
-        // required elements for PUT method.
-        var putMethodElements = this.joi.object().keys({
-            _id: this.joi.string().required(),
-            _rev: this.joi.string().required()
-        });
 
-        this.tripSchemaPost = trip;
-        this.tripSchemaPUT = putMethodElements.concat(trip);
+        this.tripSchemaPUT = tripSchema.keys({id: this.joi.string().required()});
+        
+        this.tripSchemaPost = tripSchema;
 
         // images validation
         this.imageSchemaPost = this.hoek.clone(this.imageValidation.basicImageSchema);
