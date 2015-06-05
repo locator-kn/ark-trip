27b5ebe31f12c031332f70a095acf47cad3ac110
SteffenGorenflo <SteffenGorenflo@gmail.com>
2015-06-05 12:31:36 +0200

    change required keys into optional and later to required

--- src/util/schema.ts
+++ src/util/schema.ts
@@ -23,33 +23,33 @@ class Schema {
      */
     private initSchemas():void {
 
-        // required trip schema document
+        // optional trip schema document
         var tripSchema = this.joi.object().keys({
-            title: this.joi.string().required(),
+            title: this.joi.string(),
 
             city: this.joi.object().keys({
                 title: this.joi.string().required(),
                 place_id: this.joi.string().required(),
                 id: this.joi.string().required()
-            }).required(),
+            }),
 
-            description: this.joi.string().required(),
+            description: this.joi.string(),
 
             // if equipment is provided, this key must be true
             accommodation: this.joi.when('accommodation_equipment', {
                 is: this.joi.array().required(),
                 then: this.joi.valid(true).required(),
-                otherwise: this.joi.boolean().required()
+                otherwise: this.joi.boolean()
             }),
             accommodation_equipment: this.joi.array(),
 
-            start_date: this.joi.date().min('now').required(),
-            end_date: this.joi.date().min(this.joi.ref('start_date')).required(),
+            start_date: this.joi.date().min('now'),
+            end_date: this.joi.date().min(this.joi.ref('start_date')),
 
-            persons: this.joi.number().integer().min(1).required(),
+            persons: this.joi.number().integer().min(1),
 
             // TODO: validate max days : https://github.com/hapijs/joi/issues/667
-            days: this.joi.number().integer().min(1).required(),
+            days: this.joi.number().integer().min(1),
 
             // optional keys upon creation
             description_money: this.joi.string(),
@@ -59,20 +59,12 @@ class Schema {
             delete: this.joi.boolean().default(false),
         });
 
-        var optSchema = tripSchema.optionalKeys('title', 'city', 'description',
+        var requiredSchema = tripSchema.requiredKeys('title', 'city', 'description',
             'start_date', 'end_date', 'persons', 'days', 'accommodation');
 
-        this.tripSchemaPUT = optSchema.keys({
-            // if equipment is provided, this key must be true
-            accommodation: this.joi.when('accommodation_equipment', {
-                is: this.joi.array().required(),
-                then: this.joi.valid(true).required(),
-                otherwise: this.joi.boolean()
-            })
-        }).required().min(1).description('Update Trip JSON object');
-
-
-        this.tripSchemaPost = tripSchema.required().description('Trip JSON object');
+        // exported schemas
+        this.tripSchemaPUT = tripSchema.required().min(1).description('Update Trip JSON object');
+        this.tripSchemaPost = requiredSchema.required().description('Trip JSON object');
 
         // images validation
         this.imageSchemaPost = this.hoek.clone(this.imageValidation.basicImageSchema);
