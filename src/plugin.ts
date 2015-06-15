2e84fd96ce588e95caf6d960fae3b475f50faaad
T1m1 <Ruprecht.t@gmx.de>
2015-06-15 10:08:53 +0200

    add username to trip information

--- src/plugin.ts
+++ src/plugin.ts
@@ -378,7 +378,8 @@ class Trip {
     private createTripWithPicture(request, reply) {
         // create an empty "preTrip" before uploading a picture
         var userid = request.auth.credentials._id;
-        this.db.createTrip({type: "preTrip", userid: userid}, (err, data) => {
+        var username = request.auth.credentials.name;
+        this.db.createTrip({type: "preTrip", userid: userid, username: username}, (err, data) => {
             if (err) {
                 return reply(this.boom.badRequest(err));
             }
@@ -457,6 +458,7 @@ class Trip {
 
         // get user id from authentication credentials
         request.payload.userid = request.auth.credentials._id;
+        request.payload.username = request.auth.credentials.name;
         request.payload.type = 'trip';
 
         this.db.createTrip(request.payload, (err, data) => {
